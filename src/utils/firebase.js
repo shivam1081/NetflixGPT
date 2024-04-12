640f107f38849d9f8f801eb97540cbfba6c4d99f
Shivam Dubey <shivamdubey1081@gmail.com>
2024-04-12 11:28:25 +0530

    Deployment done on new project in firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA5rbk52iOR2Sxs1VSBcjfpYiZWgO8DUso",
-  authDomain: "netflixgpt-21ac9.firebaseapp.com",
-  projectId: "netflixgpt-21ac9",
-  storageBucket: "netflixgpt-21ac9.appspot.com",
-  messagingSenderId: "1075191481006",
-  appId: "1:1075191481006:web:a3acd413c462527ce3233f",
-  measurementId: "G-85YE2X48WB"
+  apiKey: "AIzaSyCmpldg4a2URzKYf0D_BSbGIY7RrNnrMv4",
+  authDomain: "netflixgpt-shivam.firebaseapp.com",
+  projectId: "netflixgpt-shivam",
+  storageBucket: "netflixgpt-shivam.appspot.com",
+  messagingSenderId: "511230375777",
+  appId: "1:511230375777:web:47037b59f1df5178e83720",
+  measurementId: "G-PQZXH70518"
 };
 
 // Initialize Firebase
